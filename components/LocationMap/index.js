ca5c825e4d390fd17c359667af9950a12a5d723c
Sebastian Meza <sbstnmz@gmail.com>
2022-06-21 21:31:05 -0400

    remove comments

--- components/LocationMap/index.js
+++ components/LocationMap/index.js
@@ -17,26 +17,4 @@ const LocationMap = withScriptjs(
   ))
 );
 
-// => (
-//   <div style={{ height: "300px", width: "100%" }}>
-//     <GoogleMap
-//       defaultZoom={15}
-//       defaultCenter={{ lat: -33.6829258, lng: -71.2161594 }}
-//     >
-//       <Marker position={{ lat: -33.6829258, lng: -71.2161594 }} />
-//     </GoogleMap>
-//   </div>
-// <div style={{ height: "300px", width: "100%" }}>
-//   <GoogleMapReact
-//     bootstrapURLKeys={{
-//       key: "AIzaSyDAk3d99G0eD62J5RpLkixQGI2oDjJOQ4g",
-//       language: "es",
-//     }}
-//     defaultCenter={defaultProps.center}
-//     defaultZoom={defaultProps.zoom}
-//   >
-//     <AnyReactComponent lat={-33.6829258} lng={-71.2161594} text="My Marker" />
-//   </GoogleMapReact>
-// </div>
-
 export default LocationMap;

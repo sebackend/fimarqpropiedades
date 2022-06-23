4403af959d8545b33df0d8cbb2a40adfe17f96c3
Sebastian Meza <sbstnmz@gmail.com>
2022-06-23 02:52:52 -0400

    setup lang = 'es' for html attribute

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   experimental: { images: { layoutRaw: true } },
+  i18n: {
+    locales: ["es"],
+    defaultLocale: "es",
+  },
 };
 
 module.exports = nextConfig;

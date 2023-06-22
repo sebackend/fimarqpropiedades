aacfea9eaecc1f191deadf72faea01857c3dd76c
Sebastian Meza <sbstnmz@gmail.com>
2023-06-22 18:48:40 -0400

    add: display markdown from richText content into acerca-de page

--- components/PropertyDetail/index.js
+++ components/PropertyDetail/index.js
@@ -1,8 +1,22 @@
 export default function PropertyDetail({ property }) {
+  const { fotos: photos } = property.attributes;
+
+  console.log("photos");
+  console.log();
+
   return (
     <section className="row">
       <div className="col-12">
         <h1 className="text-center">{property.attributes.titulo}</h1>
+      </div>
+
+      <div className="col-6 mt-5">
+        {/* <ImageGallery
+          items={photos.data.map((photo) => ({
+            original: photo.attributes.url,
+            thumbnail: photo.attributes.url,
+          }))}
+        /> */}
       </div>
     </section>
   );
--- pages/acerca-de.js
+++ pages/acerca-de.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import ReactMarkdown from "react-markdown";
+import markdownToHtml from "../lib/markdownToHtml";
 
-export default function About({ company }) {
+export default function About({ company, aboutUs }) {
   return (
     <div>
       <Head>
@@ -11,8 +11,8 @@ export default function About({ company }) {
       <main>
         <section className="row mt-5">
           <div className="col-12 p-5">
-            {company?.acerca_de ? (
-              <ReactMarkdown>{company.acerca_de}</ReactMarkdown>
+            {aboutUs.length ? (
+              <div dangerouslySetInnerHTML={{ __html: aboutUs }}></div>
             ) : null}
           </div>
         </section>
@@ -29,13 +29,21 @@ export const getStaticProps = async () => {
   });
 
   const data = await response.json();
+  const company =
+    data?.data && data.data[0] && data.data[0]?.attributes
+      ? data.data[0].attributes
+      : {};
+  const aboutUs =
+    Object.keys(company).length === 0
+      ? ""
+      : await markdownToHtml(company.acerca_de);
+
+  console.log(aboutUs);
 
   return {
     props: {
-      company:
-        data?.data && data.data[0] && data.data[0]?.attributes
-          ? data.data[0].attributes
-          : {},
+      company,
+      aboutUs,
     },
     revalidate: 150,
   };

c96685db699d321ab4059b60ab67ec0d6cd7103e
Sebastian Meza <sbstnmz@gmail.com>
2023-06-23 15:36:56 -0400

    add: display detalle rich text content from property detail view

--- components/PropertyDetail/index.js
+++ components/PropertyDetail/index.js
@@ -1,23 +1,34 @@
 export default function PropertyDetail({ property }) {
   const { fotos: photos } = property.attributes;
 
-  console.log("photos");
-  console.log();
+  console.log("property");
+  console.log(property.attributes.detalle);
 
   return (
-    <section className="row">
-      <div className="col-12">
-        <h1 className="text-center">{property.attributes.titulo}</h1>
-      </div>
+    <>
+      <section className="row">
+        <div className="col-12">
+          <h1 className="text-center">{property.attributes.titulo}</h1>
+        </div>
 
-      <div className="col-6 mt-5">
-        {/* <ImageGallery
-          items={photos.data.map((photo) => ({
-            original: photo.attributes.url,
-            thumbnail: photo.attributes.url,
-          }))}
-        /> */}
-      </div>
-    </section>
+        <div className="col-6 mt-5">
+          {/* <ImageGallery
+            items={photos.data.map((photo) => ({
+              original: photo.attributes.url,
+              thumbnail: photo.attributes.url,
+            }))}
+          /> */}
+        </div>
+      </section>
+      <section className="row mt-5">
+        <div className="col-12 p-5">
+          {property.attributes.detalle.length ? (
+            <div
+              dangerouslySetInnerHTML={{ __html: property.attributes.detalle }}
+            ></div>
+          ) : null}
+        </div>
+      </section>
+    </>
   );
 }
